d7fe966f76d3e8c7d63ec1b798c4c724e36c2e14
iqra-ali <iqra.ali@student.moringaschool.com>
2023-05-16 08:58:47 +0300

    squareroot and power operations

--- calculator.py
+++ calculator.py
@@ -19,21 +19,24 @@ def power(a, b):
     return a ** b
 
 opt = int(input("Choose Operation from 1(Add), 2(Sub), 3(Multi), 4(Div), 5(Square Root), 6(Power) = "))
-n1 = int(input("First Number        = "))
-n2 = int(input("Second Number = "))
+n1 = int(input("First Number = "))
 
 if opt == 1:
+    n2 = int(input("Second Number = "))
     print(n1, ' + ', n2, '  =  ', addition(n1, n2))
 elif opt == 2:
+    n2 = int(input("Second Number = "))
     print(n1, ' - ', n2, '  =  ', subtraction(n1, n2))
 elif opt == 3:
+    n2 = int(input("Second Number = "))
     print(n1, ' * ', n2, '  =  ', multiplication(n1, n2))
 elif opt == 4:
+    n2 = int(input("Second Number = "))
     print(n1, ' / ', n2, '  =  ', division(n1, n2))
 elif opt == 5:
     print('Square Root of', n1, ' = ', square_root(n1))
-    print('Square Root of', n2, ' = ', square_root(n2))
 elif opt == 6:
+    n2 = int(input("Power = "))
     print(n1, ' ^ ', n2, '  =  ', power(n1, n2))
 else:
     print('Invalid Input')
